8a08d28b50684013ec7aa1214c315160a02dcc7d
Kathryn Clark <kathrynlael@gmail.com>
2020-03-04 19:38:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,6 +16,10 @@ def add_element_to_start_of_array(array, element)
  array.unshift("wow")
 end
 
+def remove_element_from_end_of_array(array)
+  array = ["I", "am", "really", "learning", "arrays!"]
+  arrays! = array.pop
+end
 
 def remove_element_from_start_of_array(array)
   
