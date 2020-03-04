a8b0af38acd2d3ce3bc11fa4e54d943ac258a488
Kathryn Clark <kathrynlael@gmail.com>
2020-03-04 19:41:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,21 +23,22 @@ end
 
 def remove_element_from_start_of_array(array)
   array=["wow", "I", "am", "really", "learning", "arrays!"]
+  array.shift("wow")
 end
 
 def retrieve_element_from_index(array, index_number)
-array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
   
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
 end
